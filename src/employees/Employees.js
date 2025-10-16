b9de01dc8c6b23e21d80f81fa035f2e2a415476b
top-secret666 <>
2025-10-16 08:10:40 +0300

    refactor:Share responsibility->Employees - clean view

--- src/employees/Employees.js
+++ src/employees/Employees.js
@@ -1,15 +1,10 @@
-import EmployeeAPI from '../services/employeeService';
 import Table from './Table';
 
 const Employees = () => {
-  const employeesData = EmployeeAPI.all();
-
   return (
     <div>
       <h2>Сотрудники</h2>
       <Table data={employeesData} />
     </div>
   );
-};
-
-export default Employees;
+}
